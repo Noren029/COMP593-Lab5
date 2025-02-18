1c1bfbf5630a00edfd9e5491ecb2bc67fde0a25f
Norlon Sibug <johndoe@example.com>
2025-02-18 11:12:36 -0500

    Update pastebinapi.py

--- pastebinapi.py
+++ pastebinapi.py
@@ -1,3 +1,16 @@
+'''log_matching.py - Module to help analyze a log file using regular expressions.
+COMP 593 Scripting Applications - Winter 2025 (Week 5)
+Louis Bertrand <louis.bertrand@flemingcollege.ca>
+
+Usage: Import this module into your main program
+
+### STUDENTS: PLEASE ADD THE STANDARD ACADEMIC INTEGRITY STATEMENT.###
+# This program is strictly my own work. Any material beyond course learning
+# materials that is taken from the Web or other sources is properly cited,
+# giving credit to the original author(s).
+
+'''
+
 import requests
 import sys
 
